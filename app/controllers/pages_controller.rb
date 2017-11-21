5850608af8d3b4d7a9162ecaffeeffb767060f74
David Kalanda <davidmkalanda@gmail.com>
2017-11-21 02:01:39 +0000

    Disable side bar on About and Contact pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   def about
     @about = About.first
+    @disable_sidebar = true
     # @categories = Category.all
   end
 
   def contact
     @contact= Contact.all.first
+    @disable_sidebar = true
     # @categories = Category.all
   end
 end
